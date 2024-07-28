3e9da71c1dbb528ea89fc52b0ceca306bcac2ab9
AlejandroCarpioH <david242455@gmail.com>
2024-07-28 18:13:39 -0400

    new add

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,30 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import "./App.css";
-import { getApi } from "./api/getData";
+// import { getApi } from "./api/getData";
 
-interface data {
-  completed: boolean;
-  id: number;
-  title: string;
-  userId: number;
-}
+// interface data {
+//   completed: boolean;
+//   id: number;
+//   title: string;
+//   userId: number;
+// }
 
 function App() {
-  const [users, setUsers] = useState<data[]>([]);
+  // const [users, setUsers] = useState<data[]>([]);
 
-  useEffect(() => {
-    (async () => {
-      const data = (await getApi()) as data[];
-      setUsers(data);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const data = (await getApi()) as data[];
+  //     setUsers(data);
+  //   })();
+  // }, []);
 
   return (
     <>
-      <div>
-        {users.map((user) => {
-          return (
-            <div style={{ display: "grid", gap: "1rem" }}>
-              <p>{user.id}</p>
-              <p>{user.title}</p>
-              <p>{user.completed}</p>
-              <p>{user.userId}</p>
-              <p>hola alejandro 2</p>
-            </div>
-          );
-        })}
-      </div>
+      <div>hola nuevo build desde local ahora si</div>
     </>
   );
 }
--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,6 +1,5 @@
 export const getApi = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
   const data = await response.json();
-
   return data;
 };
