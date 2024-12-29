4442d6249609ddf2ace575da28d79b220889ead6
AlejandroCarpioH <david242455@gmail.com>
2024-12-29 18:52:05 -0300

    add file audio

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,12 @@ import "./App.css";
 
 function App() {
   const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
   const [audio, setAudio] = useState<File | null>(null);
 
   const handleFetch = async () => {
+    setValue("");
+    setLoading(true);
     const formData = new FormData();
     if (!audio) return;
     formData.append("audio", audio);
@@ -27,40 +30,58 @@ function App() {
       body: formData,
     });
     console.log(response);
-    if (!response.ok) return;
+    if (!response.ok) {
+      setLoading(false);
+      return;
+    }
+
     const result = (await response.json()) as { message: string };
     setValue(result.message);
-
+    setLoading(false);
     console.log(result);
   };
 
   return (
     <>
-      <div className="container">
-        <input
-          type="file"
-          accept="audio/*"
-          onChange={(e) => {
-            if (!e.target.files) return;
-            setAudio(e.target.files[0]);
-          }}
-        />
-        <button onClick={handleFetch}>enviar</button>
-        <textarea
-          value={`
+      {!loading ? (
+        <div className="container">
+          <input
+            // style={{ height: 80 }}
+            type="file"
+            accept="audio/*"
+            onChange={(e) => {
+              if (!e.target.files) return;
+              setAudio(e.target.files[0]);
+            }}
+          />
+          <button
+            style={{
+              height: 80,
+              fontSize: "2rem",
+              backgroundColor: "rgb(48, 128, 28)",
+            }}
+            onClick={handleFetch}
+          >
+            enviar
+          </button>
+          <textarea
+            value={`
           ${value}
             `}
-          readOnly
-          style={{ height: 100, resize: "none" }}
-        />
-        {/* <input
+            readOnly
+            style={{ height: 100, resize: "none", fontSize: "1.5rem" }}
+          />
+          {/* <input
           style={{ height: 200 }}
           type="text"
           value={"hola como estas "}
           readOnly
           placeholder="respuesta..."
         /> */}
-      </div>
+        </div>
+      ) : (
+        <div>cargando...</div>
+      )}
     </>
   );
 }
